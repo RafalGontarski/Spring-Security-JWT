9ff1e9c9444227c04234fa584ee8fb70cbd5fbe8
RafalGontarski <gontarskirafal@gmail.com>
2023-06-02 21:17:53 +0200

    JJwt dependencies has been added and getSignInKey method implemented.

---
+++ src/main/java/com/codeMaker/security/config/JwtService.java
@@ -0,0 +1,32 @@
+package com.codeMaker.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "77217A25432A462D4A614E645267556B58703272357538782F413F4428472B4B";
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
