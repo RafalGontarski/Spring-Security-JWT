264ba0522a8ed59bf7bc4b8466c284d778481bb6
RafalGontarski <gontarskirafal@gmail.com>
2023-06-02 21:21:55 +0200

    Method extractClaims has been implemented.

--- src/main/java/com/codeMaker/security/config/JwtService.java
+++ src/main/java/com/codeMaker/security/config/JwtService.java
@@ -7,13 +7,19 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "77217A25432A462D4A614E645267556B58703272357538782F413F4428472B4B";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
