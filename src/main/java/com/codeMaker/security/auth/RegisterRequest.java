087ca83b0c9a918006e83b3164e393bcbdf2b291
RafalGontarski <gontarskirafal@gmail.com>
2023-06-03 17:32:13 +0200

    Class RegisterRequest has been created and implemented.

---
+++ src/main/java/com/codeMaker/security/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.codeMaker.security.auth;
+
+import com.codeMaker.security.user.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
