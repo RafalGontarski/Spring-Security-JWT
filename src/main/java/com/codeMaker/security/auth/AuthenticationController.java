bbc26047745e1bc8cc9c6f0417d98183bb97da51
RafalGontarski <gontarskirafal@gmail.com>
2023-06-03 17:26:23 +0200

    Class AuthenticationController has register and authenticate endpoints.

---
+++ src/main/java/com/codeMaker/security/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.codeMaker.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+
+    }
+
+
+}
