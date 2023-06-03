8a9cb12a2509569e1c8f51a8590cac0f41ae5c73
RafalGontarski <gontarskirafal@gmail.com>
2023-06-03 17:36:00 +0200

    Class AuthenticationService initialize.

---
+++ src/main/java/com/codeMaker/security/auth/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.codeMaker.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service@RequiredArgsConstructor
+public class AuthenticationService {
+}
